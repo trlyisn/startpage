27a997d7329b0290b9c95bd76ea741f1b7cc9896
trlyisn <150703511+trlyisn@users.noreply.github.com>
2026-02-24 08:41:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,34 +13,3 @@ async function getWeather() {
 
 getWeather();
 
-const targetDate = new Date("March 8, 2026 11:05:00").getTime();
-
-function updateCountdown() {
-  const now = new Date().getTime();
-  const distance = targetDate - now;
-
-  if (distance < 0) {
-    document.getElementById("countdown-data").innerText = "TAKEOFF";
-    return;
-  }
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  const d = days.toString().padStart(2, "0");
-  const h = hours.toString().padStart(2, "0");
-  const m = minutes.toString().padStart(2, "0");
-  const s = seconds.toString().padStart(2, "0");
-
-  document.getElementById(
-    "countdown-data"
-  ).innerText = `${d}d ${h}h ${m}m ${s}s`;
-}
-
-setInterval(updateCountdown, 1000);
-
-updateCountdown();
